3d5939b8272fb4d51f4de0d95dddb3f7975b003c
Julian Della Bosca <julian.dellabosca@gmail.com>
2025-11-22 21:20:09 +1100

    Fix mob routes - remove duplicate /mobs prefix

--- api/routes/mobs.js
+++ api/routes/mobs.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const mobController = require('../controllers/mobController');
 
-// Mob routes
-router.get('/mobs', mobController.getAllMobs);
-router.get('/mobs/:id', mobController.getMobById);
-router.get('/mobs/:id/history', mobController.getMobHistory);
-router.post('/mobs', mobController.createMob);
-router.put('/mobs/:id', mobController.updateMob);
+// Mob routes (no /mobs prefix - already mounted at /mobs in index.js)
+router.get('/', mobController.getAllMobs);
+router.get('/:id', mobController.getMobById);
+router.get('/:id/history', mobController.getMobHistory);
+router.post('/', mobController.createMob);
+router.put('/:id', mobController.updateMob);
 
 // Farm statistics
-router.get('/farm/statistics', mobController.getFarmStatistics);
+router.get('/farm-statistics', mobController.getFarmStatistics);
 
 // Breeding events
 router.post('/breeding-events', mobController.recordBreedingEvent);
